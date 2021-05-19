26ee62f2e319dece0410b74d5f184dc247875760
Hoollow <mateuszkowalewski12@gmail.com>
2021-05-19 13:33:20 +0200

    Front cz1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DemoApisComponent } from './demo-apis/demo-apis.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'demo-apis', component: DemoApisComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
